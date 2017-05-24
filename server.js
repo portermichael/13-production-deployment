07ca5ee92093c6826e18bd4b9e2535739a43d61a
mikeporter <porter.michael.james@gmail.com>
2017-05-24 13:36:22 -0700

    configuring constr for heroku

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const requestProxy = require('express-request-proxy'); // REVIEW: We've added a 
 const PORT = process.env.PORT || 3000;
 const app = express();
 // const conString = 'postgres://USERNAME:PASSWORD@HOST:PORT';
-const conString = 'postgres://postgres:passwordhere@localhost:5432/kilovolt'; // done2: Don't forget to set your own conString
+const conString = 'postgres://postgres:passwordhere@lab13code-may24.herokuapp.com'; // done2: Don't forget to set your own conString
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', err => console.error(err));
